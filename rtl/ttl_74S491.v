da864a7cda30ebd96981649339e04c708619600f
Romain Dolbeau <romain@dolbeau.org>
2025-09-28 13:44:32 +0200

    OE -> OE_n

--- rtl/ttl_74S491.v
+++ rtl/ttl_74S491.v
@@ -19,8 +19,8 @@ module ttl_74S491(input D0,
 		  inout Q7_n,
 		  inout Q8_n,
 		  inout Q9_n,
-		  input  CLK,
-		  input  OE);
+		  input CLK,
+		  input OE_n);
 
    reg [9:0] count;
 
@@ -41,16 +41,16 @@ module ttl_74S491(input D0,
 	      end
      end
    
-   assign Q0_n = ~OE ? count[0] : 1'bz;
-   assign Q1_n = ~OE ? count[1] : 1'bz;
-   assign Q2_n = ~OE ? count[2] : 1'bz;
-   assign Q3_n = ~OE ? count[3] : 1'bz;
-   assign Q4_n = ~OE ? count[4] : 1'bz;
-   assign Q5_n = ~OE ? count[5] : 1'bz;
-   assign Q6_n = ~OE ? count[6] : 1'bz;
-   assign Q7_n = ~OE ? count[7] : 1'bz;
-   assign Q8_n = ~OE ? count[8] : 1'bz;
-   assign Q9_n = ~OE ? count[9] : 1'bz;
+   assign Q0_n = ~OE_n ? count[0] : 1'bz;
+   assign Q1_n = ~OE_n ? count[1] : 1'bz;
+   assign Q2_n = ~OE_n ? count[2] : 1'bz;
+   assign Q3_n = ~OE_n ? count[3] : 1'bz;
+   assign Q4_n = ~OE_n ? count[4] : 1'bz;
+   assign Q5_n = ~OE_n ? count[5] : 1'bz;
+   assign Q6_n = ~OE_n ? count[6] : 1'bz;
+   assign Q7_n = ~OE_n ? count[7] : 1'bz;
+   assign Q8_n = ~OE_n ? count[8] : 1'bz;
+   assign Q9_n = ~OE_n ? count[9] : 1'bz;
    
 endmodule // ttl_74S491
 
