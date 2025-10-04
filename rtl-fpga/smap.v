259ef1355655f7b2b1133d4b36ec3c00c82de6a6
Romain Dolbeau <romain@dolbeau.org>
2025-10-04 15:24:19 +0200

    make pmap, smap parametrized (Sun-3 uses a similar scheme)

--- rtl-fpga/smap.v
+++ rtl-fpga/smap.v
@@ -1,15 +1,16 @@
 `timescale 1ns / 1ns
-module smap_sram(input CLK,
-		 input [11:0] idx,
-		 input 	      WR,
-		 input [7:0]  ia_in,
-		 output [7:0] ia_out
-		 );
-
-   sram_sync #(.DATA_WIDTH(8), .IDX_WIDTH(12)) smap (.CLK(CLK),
-						  .idx(idx),
-						  .WR(WR),
-						  .din(ia_in),
-						  .dout(ia_out)
-						  );
+module smap_sram
+  #(parameter DATA_WIDTH=8, IDX_WIDTH=12)
+   (input CLK,
+    input [IDX_WIDTH-1:0]   idx,
+    input 		    WR,
+    input [DATA_WIDTH-1:0]  ia_in,
+    output [DATA_WIDTH-1:0] ia_out
+    );
+   
+   sram_sync #(.DATA_WIDTH(DATA_WIDTH), .IDX_WIDTH(IDX_WIDTH)) smap (.CLK(CLK),
+								     .idx(idx),
+								     .WR(WR),
+								     .din(ia_in),
+								     .dout(ia_out)						  );
 endmodule; // smap_sync
