d37b8d1d504829358879fcac81aa4ed99f3e6d05
Romain Dolbeau <romain@dolbeau.org>
2025-10-04 10:45:49 +0200

    add ability to generate 16-bits wide ROM

--- rtl/rom.c
+++ rtl/rom.c
@@ -12,17 +12,26 @@ void usage()
 
 int main(int argc, char *argv[])
 {
-    int ret, low = 0, a = 0;;
+  int ret, low = 0, a = 0, wide = 0;
     if (argc != 2) usage();
     if (argv[1][0] == '0') low = 1;
     if (argv[1][0] == '1') low = 0;
+    if (argv[1][0] == 'X') wide = 1;
     ret = read(0, data, sizeof(data));
     if (ret > 0) {
+      if (!wide) {
         for (int o = low; o < ret; o += 2) {
-	    unsigned char b = data[o];
-	    printf("    15'h%04x: out = 8'h%02x;\n", a, b);
-	    a++;
+	  unsigned char b = data[o];
+	  printf("    15'h%04x: out = 8'h%02x;\n", a, b);
+	  a++;
 	}
+      } else {
+        for (int o = 0; o < ret/2; o++) {
+	  unsigned short w = ((unsigned short*)data)[o];
+	  printf("    15'h%04x: dout <= 16'h%04x;\n", o, ((w>>8)&0xFF)|((w<<8)&0xFF00));
+	}
+	
+      }
     }
     return 0;
 }
